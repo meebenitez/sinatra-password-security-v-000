8c93306c213c8a6530bde8258db7694e32bccbac
Mary Benitez <meebenitez@gmail.com>
2017-10-21 16:52:07 +0000

    automatically backed up by learn

--- db/migrate/20150916154312_create_users.rb
+++ db/migrate/20150916154312_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def up
     create_table :users do |t|
       t.string :username
       t.string :password
     end
+  end
 
+  def down
+    drop_table :users
   end
 end
